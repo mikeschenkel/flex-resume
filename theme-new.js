144bf41423b6c3a1e80d7af2646e6841131bd9ab
Mike Schenkel <mikeschenkel@gmail.com>
2017-06-29 21:56:17 +0200

    Update and rename themes-new.js to theme-new.js

--- theme-new.js
+++ theme-new.js
@@ -9,11 +9,11 @@ const config = require('./config');
 const dirPath = path.join(__dirname, config.paths.src, config.paths.themes, args[2]);
 
 mkdirp(dirPath, function(err) {
-    if (err) throw err;
+  if (err) throw err;
     console.log('dir(s) created');
     var blankTheme = path.join(__dirname, config.paths.src, config.paths.themes, 'blank');
     ncp(blankTheme, dirPath, function (err) {
-        if (err) throw err;
+      if (err) throw err;
         console.log('created blank theme');
-    })
-})
+    });
+});
